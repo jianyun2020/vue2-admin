2201842233ca5fa47a71723ad7480b3609464d2a
jianyun2020 <sujianyun2020@163.com>
2021-03-31 13:39:07 +0800

    update

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'normalize.css'
 
+import '@/style/index.scss'
 import './icons' // icon
 
 Vue.use(ElementUI)
