87667e0cb8c5f9f423fec35366eb8cbe2a9d7504
jianyun2020 <sujianyun2020@163.com>
2021-04-08 11:41:38 +0800

    update

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>
-    Sidebar
+  <div :class="{'has-logo': showLogo}">
+    <logo v-if="showLogo" :collapse="isCollapse"/>
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+        :default-active="activeMenu"
+        :collapse="isCollapse"
+        :background-color="variables.menuBg"
+        :text-color="variables.menuText"
+        :unique-opened="false"
+        :active-text-color="variables.menuActiveText"
+        :collapse-transition="false"
+        mode='vertical'
+      >
+        <sidebar-item v-for="route in permission_routes" :key="route.path" :item="route" :base-path="route.path" />
+      </el-menu>
+    </el-scrollbar>
   </div>
 </template>
+
+<script>
+import Logo from './Logo'
+import SidebarItem from './SiderbarItem'
+
+export default {
+  components: {
+    Logo,
+    SidebarItem
+  }
+}
+</script>
