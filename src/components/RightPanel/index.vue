554079e7070cdcdf60c633c81238f31c84ccae90
jianyun2020 <sujianyun2020@163.com>
2021-04-12 10:16:57 +0800

    update

---
+++ src/components/RightPanel/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div ref="rightPanel" :class="{show: show}" class="right-Panel-container">
+    <div class="rightPanel-background" />
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.showRightPanel {
+  overflow: hidden;
+  position: relative;
+  width: calc(100% - 15px);
+}
+</style>
+
+<style lang="scss" scoped>
+.rightPanel-background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  opacity: 0;
+  transition: opacity .3s cubic-bezier(.7, .3, .1, 1);
+  background: rgba(0, 0, 0, .2);
+  z-index: -1;
+}
+</style>
