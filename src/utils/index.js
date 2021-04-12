de261058164041bf7180fce04121645a9a830b67
jianyun2020 <sujianyun2020@163.com>
2021-04-12 10:54:03 +0800

    add

---
+++ src/utils/index.js
@@ -0,0 +1,39 @@
+
+/**
+ * Check if an element has a class
+ *
+ * @export
+ * @param {HTMLElement} ele
+ * @param {String} cls
+ * @return {Boolean} 
+ */
+export function hasClass (ele, cls) {
+  return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'))
+}
+
+/**
+ * Add class to element
+ *
+ * @export
+ * @param {HTMLElement} ele
+ * @param {String} cls
+ */
+export function addClass (ele, cls) {
+  if (!hasClass(ele, cls)) {
+    ele.className += ' ' + cls
+  }
+}
+
+/**
+ * Remove class from element
+ *
+ * @export
+ * @param {HTMLElement} ele
+ * @param {String} cls
+ */
+export function removeClass (ele, cls) {
+  if (hasClass(ele, cls)) {
+    const reg = new RegExp('(\\s|^)' + cls + '(\\s|$)')
+    ele.className = ele.className.replace(reg, ' ')
+  }
+}
