53e84193ae3cb9fc1859d1db1c8a43fba5c1cb01
jianyun2020 <sujianyun2020@163.com>
2021-04-16 18:10:48 +0800

    update

---
+++ src/views/dashboard/admin/index.vue
@@ -0,0 +1,8 @@
+<!--
+ * @Author: jianyun2020
+ * @Date: 2021-04-16 17:07:23
+ * @LastEditTime: 2021-04-16 17:08:31
+ * @Description: 权限为admin的Dashboard页面
+ * @FilePath: \vue2-admin\src\views\dashboard\admin\index.vue
+-->
+
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,5 +1,39 @@
+<!--
+ * @Author: jianyun2020
+ * @Date: 2021-04-15 15:25:25
+ * @LastEditTime: 2021-04-16 17:08:16
+ * @Description: Dashboard
+ * @FilePath: \vue2-admin\src\views\dashboard\index.vue
+-->
 <template>
   <div class="dashboard-container">
-    dashboard
+    <component :is="currentRole" />
   </div>
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+import adminDashboard from './admin'
+import editorDashboard from './editor'
+
+export default {
+  name: 'Dashboard',
+  components: {
+    adminDashboard,
+    editorDashboard
+  },
+  data () {
+    return {
+      currentRole: 'editorDashboard'
+    }
+  },
+  computed: {
+    ...mapGetters(['roles'])
+  },
+  created () {
+    if (!this.roles.includes('admin')) {
+      this.currentRole = 'editorDashboard'
+    }
+  }
+}
+</script>
